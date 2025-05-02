d361f1a20f4d54c4cc5d7bbb15f4ba33718b1317
MiloOliveira <miloliveira143@gmail.com>
2025-05-01 22:08:20 -0300

    Second commit

---
+++ main.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <string.h>
+#define pontosstatus 20
+#define limitestatus 5
+#define classesmax 4
+#define racamax 4
+#define qtdbonusraca 2
+
+//------/-------------------------------------------------------------------------//
+int SetVesselStatus() {
+    int statusp[6];
+    for (int cc = 0; cc < 6; cc++) {
+        statusp[cc] = 0;
+    }
+}
+//---------------------------------------------------------------------------------------//
+int main()
+{
+    //ordem: Clérigo, Guerreiro, Bardo, Ladino//
+    //status base//
+    int statusbaseclasse[3][classesmax] = {
+        {12, 20, 16, 14},
+        {20, 12, 16, 14},
+        {14, 16, 15, 15}
+    };
+
+   //bonus de raça//
+   //ordem: Sireno, Polar, Gatuno, Harpia//
+        //atributo
+    int bdr[qtdbonusraca][classesmax] = {
+        {1, 0, 1, 4},
+        {4, 2, 3, 3}
+    };
+        //valor//
+    int vbdr[qtdbonusraca][classesmax] = {
+        {2, 1, 3, 2},
+        {3, 4, 2, 3}
+    };
+    
+    char statusnome[6][10] = {
+        "Força",
+        "Destreza",
+        "Vigor",
+        "Intelecto",
+        "Sabedoria",
+        "Carisma"
+        
+    };
+    char atrnome[3][20] = {
+        "Vida máxima",
+        "Mana máxima",
+        "Classe de Armadura"
+    };
+    char caracteristicas[3][10] = {
+        "Nome",
+        "Raça",
+        "Classe"
+    };
+    
+    char racas[racamax][15] = {
+        "Sireno",
+        "Polar",
+        "Gatuno",
+        "Harpia"
+    };
+    
+    char classes[classesmax][15] = {
+        "Clérigo",
+        "Guerreiro",
+        "Bardo",
+        "Ladino"
+    };
+    
+    //status do player//
+    char caracp[3][20];
+    //hp, mana, ca//
+    int atrb[3];
+    //for, dex, vig int, sab, car//
+    int statusp[6];
+    
+    //operações e variáveis sistemicas//
+    int ee, ea, ec = 0;
+    int checkr, checks;
+    int esccl, escr;
+    
+    for (int cc = 0; cc < 6; cc++) {
+        statusp[cc] = 0;
+    }
+    
+    printf("Qual o nome de seu personagem? Máximo de 20 caracteres \n");
+    fgets(caracp[0], sizeof(caracp[0]), stdin);
+  
+  //classe//
+    printf("Qual será sua classe? \n");
+    for (int gg = 0; gg < classesmax; gg++) {
+        printf("\t %d. %s \n", gg + 1, classes[gg]);
+    }
+    scanf("%d",&esccl);
+    for (int hh = 0; hh < 3; hh++) {
+        atrb[hh] = statusbaseclasse[hh][esccl - 1];
+    }
+    strcpy(caracp[2],classes[esccl - 1]);
+    
+    //atributos//
+    printf("Defina seus atributos. Você tem %d pontos para dividir entre todos. \n", pontosstatus);
+    checks = pontosstatus;
+    
+    
+    for (ee = 0; ee < 6 && ec == 0; ee++) {
+        printf("Quantos pontos colocará em %s? Pode, no máximo %d \n", statusnome[ee], limitestatus);
+        printf("Você tem %d pontos restantes. \n", checks);
+        scanf("%d", &ea);
+        if (ea > limitestatus || ea < 0){
+            printf("Opção inválida. Digite outra vez, corretamente agora. \n");
+            ee--;
+            continue;
+        } else {
+            statusp[ee] += ea;
+            checks -= statusp[ee];
+        }
+        
+        
+        }
+        if (checks > 0) {
+            printf("Houve sobra de pontos. Redistribua corretamente, por favor. se não usar esses pontos agora, você os perderá. \n");
+            ee = 0;
+        } 
+    
+    
+    printf("Qual será sua raça? \n");
+    for (int ii = 0; ii < racamax; ii++) {
+      printf("\t %d.%s. Bônus em %s e %s. \n",ii + 1, racas[ii], statusnome[bdr[0][ii]], statusnome[bdr[1][ii]]);  
+    }
+    
+    //raça//
+    do {
+        scanf("%d",&escr);
+        switch(escr) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            statusp[bdr[0][escr - 1]] += vbdr[0][escr - 1];
+            statusp[bdr[1][escr - 1]] += vbdr[1][escr - 1];
+            checkr = 1;
+            break;
+        default: 
+        printf("Opção inválida.");
+    }
+
+    } while (checkr != 1);
+    strcpy(caracp[1],racas[escr - 1]);
+
+    //soma de atributos//
+    atrb[0] += statusp[2];
+    atrb[1] += statusp[3];
+    atrb[2] += statusp[2];
+   
+    //imprime a ficha//
+    
+    for (int ff = 0; ff < 3; ff++) {
+        printf("%s: %s \n", caracteristicas[ff],caracp[ff]);
+    }
+    for (int bb = 0; bb < 3; bb++) {
+        printf("%s: %d \n", atrnome[bb], atrb[bb]);
+    }
+    for (int aa = 0; aa < 6; aa++) {
+        printf("%s: %d \n", statusnome[aa], statusp[aa]);
+    }
+    
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -2,11 +2,51 @@ Esse código é um simples criador de personagem de RPG. ele se baseia em 3 prin
 Classe, Distribuição de status e 
 
 Etapa 1: Seleção de classe:
-Há uma matriz com os dados de todas as classes
+Há uma matriz com os dados de todas as classes. O comando seleciona e puxa os dados da coluna que equivale a classe. No código há comentários para se basear na ordem, mas escreverei aqui. statusbaseclasse[linha][coluna]
+Linhas: 
+"Vida máxima",
+"Mana máxima",
+"Classe de Armadura"  
+Colunas         
+"Clérigo",
+"Guerreiro",
+"Bardo",
+"Ladino"
+
+Isso gera um par ordenado que cadastra no array do player os dados da classe
 
 Etapa 2: Distribuição de status:
+A distribuição de status é feita um por um, sendo For, Dex, Vig, Int, Sab e Car. A pessoa tem uma quantia x de pontos para dsitribuir e é obrigatório por, pelo menos, 1 ponto em cada. Após isso, cadastra em outro array do player
 
 Etapa 3: Seleção de Raça
+Aqui, segue o mesmo esquema de par ordenado da etapa 1, porém com 2 matrizes: uma indica o status que aumentará, identificado de 0 a 5, e em outra o valor do bônus agregado.
+As colunas indicam a raça. Cada linha é um atributo diferente que é beneficiado. Ordem das colunas:
+     "Sireno",
+     "Polar",
+     "Gatuno",
+     "Harpia"
 
 Final: Impressão de todas as características nessa ordem:
 Nome:
+Raça:
+Classe:
+"Vida máxima",
+"Mana máxima",
+"Classe de Armadura"
+ "Força",
+"Destreza",
+"Vigor",
+"Intelecto",
+"Sabedoria",
+"Carisma"
+
+Caso queira usar o programa para algum projeto, para cada etapa recomendo que:
+1:
+Defina todos os #define para o seu cenário,
+Escreva todas as classes e raças nas matrizes,
+Defina todos os valores numéricos, seguindo a mesma sequência para todos (Ex.: Se a coluna 1 tem dados do clérigo,em todas as matrizes, a coluna 1 é do clérigo),
+
+2:
+Cheque se a distribuição e checagem está feita corretamente, caso mexa na quantia de pontos máxima
+3:
+A Checagem da raça é feita por um switch, então adicione um caso numérico para ser validado e, caso as raças dêem bônus em mais atributos, adicione uma linha extra de atribuição de valores extras, dessa forma: statusp[bdr[n][escr - 1]] += vbdr[n][escr - 1]; sendo n uma linha qualquer de bdr[][] e vbdr[][] que você tenha adicionado
